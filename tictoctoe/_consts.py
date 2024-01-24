b91fc6b5e8d43c75f785d76becb4c98acb0d85ea
Mohammadreza Amani <More.Amani@yahoo.com>
2024-01-24 17:17:13 +0330

    remove hard codes.

---
+++ tictoctoe/_consts.py
@@ -0,0 +1,14 @@
+class Consts:
+    Player1 = "X"
+    Player2 = "O"
+    PlayerWinsMessage = "Player %s wins! Congratulations!"
+    DrawMessage = "It's a draw!"
+    InvalidMoveMessage = "Invalid move. Try again."
+    InvalidInputMessage = "Invalid input. Please enter a number."
+    InvalidPlayerMessage = "Invalid player. Please enter a number."
+    AIWinsMessage = "You lose! Better luck next time."
+    PlayerInput = "Player %s, Enter your move (1-9): "
+    YouWinMessage = "You win! Congratulations!"
+    Xdelemeter = "-" * 8
+    Ydelemeter = " | "
+    GeneticAlgoTypeError = "Genetic algorithm type must be a GeneticAlgorithm instance. not %s"
